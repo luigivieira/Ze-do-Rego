7d487dcea5996ba02804b485f264d0d96351ae9f
Arthur Ferrai Ferreira <arthur@arthur.ferrai.nom.br>
2015-08-30 14:45:51 -0300

    Added StartDialogue script.

---
+++ unity_proj/Assets/Scripts/StartDialogue.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartDialogue : MonoBehaviour {
+
+	public TalkAction action;
+
+	public void ActivateAction(){
+		action.gameObject.SetActive(true);
+	}
+}
