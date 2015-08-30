e0d6037b124e90c211cf45ca7a666a8169906886
Vitor Navarro <vitor.navarro87@gmail.com>
2015-08-30 01:51:44 -0300

    Implement dialogue system

---
+++ unity_proj/Assets/Scripts/DialogueBox.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class DialogueBox : MonoBehaviour {
+	public Text title;
+	public GameObject content;
+	public Image avatar;
+	public Canvas canvas;
+	public TalkAction parentAction;
+	public GameObject buttonPrefab;
+
+	private List<Dialogue.Choice> choices;
+
+	void Awake (){
+		canvas.worldCamera = Camera.main;
+		choices = new List<Dialogue.Choice>();
+	}
+
+	public void SetTitle (string title) {
+		this.title.text = title;
+	}
+
+	public void AddChoice (Dialogue.Choice choice) {
+		GameObject choiceButton = GameObject.Instantiate(buttonPrefab);
+		choiceButton.transform.SetParent(content.transform, false);
+		choiceButton.transform.localScale = new Vector3(1, 1, 1);
+
+		Text btnText = choiceButton.transform.GetChild(0).GetComponent<Text>();
+		btnText.text = choice.dialogue;
+
+		this.choices.Add(choice);
+		int count = this.choices.Count-1;
+		choiceButton.GetComponent<Button>().onClick.AddListener( () => { 
+			OnChoiceClicked(count);
+		});
+	}
+
+	public void SetAvatar (Sprite avatar) {
+		this.avatar.sprite = avatar;
+	}
+
+	public void HideAvatar ()
+	{
+		this.avatar.gameObject.SetActive(false);
+	}
+
+	void OnMouseDown() {
+		Dismiss();
+	}
+
+	public void Dismiss(){
+		iTween.FadeTo(this.gameObject, iTween.Hash("alpha", 0));
+		Destroy(transform.parent.gameObject);
+	}
+
+	public void OnChoiceClicked(int choiceIndex){
+			Debug.Log ("Button clicked = " + choiceIndex);
+		Dialogue.Choice nextChoice = choices[choiceIndex];
+		parentAction.NextChoice(nextChoice);
+	}
+
+	public void CleanChoices ()
+	{
+		this.choices.Clear();
+		for(int i=0; i< content.transform.childCount;i++) {
+			Transform child = content.transform.GetChild(i);
+			child.parent = null;
+			Destroy(child.gameObject);
+		}
+	}
+}
---
+++ unity_proj/Assets/Scripts/TalkAction.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TalkAction : MonoBehaviour {
+	public DialogueFile dialogFile;
+	public string dialogName;
+	public int startIndex;
+	public GameObject dialogueBoxPrefab;
+	private DialogueBox dialogueBox;
+	private Dialogue currentDialogue;
+	private Dialogue.Choice currentChoice;
+	private Dialogue.Choice[] nextChoices;
+
+	// Use this for initialization
+	void OnEnable () {
+		DialogueManager manager = DialogueManager.LoadDialogueFile(dialogFile);
+		currentDialogue = manager.GetDialogue(dialogName);
+		Dialogue.Choice[] choices = currentDialogue.GetChoices();
+		if(choices.Length > 0 ){
+			if(choices.Length == 1) {
+				currentChoice = choices[0];
+				currentDialogue.PickChoice(currentChoice);
+				nextChoices = currentDialogue.GetChoices();
+			}
+			else {
+				nextChoices = choices;
+			}
+			SetupDialogueBox();
+			
+			this.gameObject.SetActive(false);
+		}
+	}
+
+	private Sprite GetImageFromResources(string speaker){
+		return Resources.Load<Sprite>("DialogueAvatars/"+speaker);
+	}
+
+	private void SetupDialogueBox() {
+		if(dialogueBox == null){
+			GameObject prefabInstance = GameObject.Instantiate(dialogueBoxPrefab);
+			dialogueBox = prefabInstance.transform.GetChild(0).GetComponent<DialogueBox>();
+		}
+		else {
+			dialogueBox.CleanChoices();
+		}
+		dialogueBox.parentAction = this;
+		Dialogue.Choice choice;
+		if(currentChoice == null){
+			choice = nextChoices[0];
+			foreach (var nextChoice in nextChoices) {
+				dialogueBox.AddChoice(nextChoice);
+			} 		
+		}
+		else {
+			choice = currentChoice;
+			dialogueBox.AddChoice(currentChoice);
+		}
+		dialogueBox.SetTitle(choice.speaker);
+		Sprite avatar = GetImageFromResources(choice.speaker);
+		if(avatar != null) dialogueBox.SetAvatar(avatar);
+		else dialogueBox.HideAvatar();
+	}
+
+	public void NextChoice(Dialogue.Choice nextChoice) {
+		currentDialogue.PickChoice(nextChoice);
+		nextChoices = currentDialogue.GetChoices();
+		if(nextChoices.Length > 0){	
+			currentChoice = null;
+			if(nextChoices.Length == 1) {
+				currentChoice = nextChoices[0];
+				currentDialogue.PickChoice(currentChoice);
+				nextChoices = currentDialogue.GetChoices();
+			}
+			SetupDialogueBox();
+		}
+		else {
+			dialogueBox.Dismiss();
+		}
+	}
+}
