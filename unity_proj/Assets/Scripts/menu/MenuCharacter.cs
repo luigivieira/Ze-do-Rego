465b1f0e7c8600ba14e931382db909a43b08ca0c
Vitor Navarro <vitor.navarro87@gmail.com>
2015-08-30 14:38:16 -0300

    Add menus structure

---
+++ unity_proj/Assets/Scripts/menu/MenuCharacter.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum Character{
+	ZedoRego,
+	Lili,
+	Menu
+}
+
+public class MenuCharacter : MonoBehaviour {
+	public Character selectedChar;
+	private SpriteRenderer renderer;
+
+
+	void Awake () {
+		renderer = GetComponent<SpriteRenderer>();
+	}
+
+	public void NextAnimation() {
+		Debug.Log ("NextAnimation");
+		GameObject.Find("MenuScreen").GetComponent<MenuScreen>().nextAnimation();
+	}
+	
+	#region Selection Highlight
+	void UpdateRendererColor(Color c){
+		renderer.color = c;
+	}
+	
+	void OnMouseEnter(){
+		Color c;
+		Color.TryParseHexString("AAAAAAFF", out c);
+		iTween.ValueTo(gameObject, iTween.Hash(
+			"from", renderer.color, 
+			"to", c,
+			"time", 0.1f,
+			"onupdate", "UpdateRendererColor"
+			));
+	}
+	
+	void OnMouseExit() {
+		iTween.ValueTo(gameObject, iTween.Hash(
+			"from", renderer.color, 
+			"to", Color.white,
+			"time", 0.1f,
+			"onupdate", "UpdateRendererColor"
+			));
+	}
+	#endregion
+
+	void OnMouseDown() {
+		if(selectedChar == Character.ZedoRego){
+			Debug.Log("Load zdorego about");
+			Application.LoadLevel("ZedoRego");
+		}
+		else if(selectedChar == Character.Menu){
+			Debug.Log("Load lili about");
+			Application.LoadLevel("AboutLili");
+		}
+	}
+}
---
+++ unity_proj/Assets/Scripts/menu/MenuScreen.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+enum CurrentMenuAnimation 
+{
+	Vespa,
+	AirPlane,
+	Cow,
+}
+
+public class MenuScreen : MonoBehaviour {
+	public Animator titleAnimator;
+	public Animator zedoregoVespaAnimator;
+	public Animator zedoregoPlaneAnimator;
+	public Animator liliAnimator;
+	public AudioSource bgm;
+	public AudioSource[] charAudioSources; 
+
+	private CurrentMenuAnimation currentAnimation;
+
+	void Start() {
+		currentAnimation = CurrentMenuAnimation.Cow;
+		StartCoroutine(StartAnimations());
+	}
+
+	IEnumerator StartAnimations() {
+		yield return new WaitForSeconds(2.0f);
+		titleAnimator.SetTrigger("play");
+	}
+
+	public void nextAnimation(){
+		zedoregoVespaAnimator.SetBool("play", false);
+		zedoregoPlaneAnimator.SetBool("play", false);
+		liliAnimator.SetBool("play", false);
+
+		if (currentAnimation == CurrentMenuAnimation.Cow){
+			getRandomZeDoRegoAnimation();
+			if(currentAnimation == CurrentMenuAnimation.Vespa){
+				zedoregoVespaAnimator.SetBool("play", true);
+				charAudioSources[(int)currentAnimation].Play();
+			}
+			else if(currentAnimation == CurrentMenuAnimation.AirPlane){
+				zedoregoPlaneAnimator.SetBool("play", true);
+				charAudioSources[(int)currentAnimation].Play();
+			}
+		}
+		else
+		{
+			currentAnimation = CurrentMenuAnimation.Cow;
+			liliAnimator.SetBool("play", true);
+		}
+	}
+
+	void getRandomZeDoRegoAnimation() {
+		currentAnimation = (CurrentMenuAnimation)Random.Range(0,2);
+	}
+}
